cc256137ca601b188e66d87459540fdda11564fe
Darrylmic <virgil47th@gmail.com>
2025-08-06 14:34:00 +0100

    Fixed main.dart and added theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'screens/home.dart';
 import 'screens/login.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const DaCloudHub());
+void main() {
+  runApp(MyApp());
 }
 
-class DaCloudHub extends StatelessWidget {
-  const DaCloudHub({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,18 +15,7 @@ class DaCloudHub extends StatelessWidget {
         colorSchemeSeed: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          if (snapshot.hasData) {
-            return const HomeScreen();
-          }
-          return const LoginScreen();
-        },
-      ),
+      home: LoginScreen(),
     );
   }
 }
